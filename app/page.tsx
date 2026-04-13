fb4709d8397e3f4df09be6d29fc2e1a5ed97c934
Jesus Toledo <jesus_toledo@epamneoris.com>
2026-04-12 23:08:16 -0700

    fix fetch production

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 async function getProducts() {
   const res = await fetch('https://fakestoreapi.com/products', {
     cache: 'no-store',
@@ -11,36 +13,46 @@ async function getProducts() {
 }
 
 export default async function Home() {
-  const products = await getProducts();
+  let products = [];
+
+  try {
+    products = await getProducts();
+  } catch (error) {
+    console.error(error);
+  }
 
   return (
     <main className="min-h-screen bg-gray-100 p-10">
       <h1 className="text-4xl font-bold text-center mb-10">
-        🛍️ Catálogo de Productos
+        Catálogo de Productos
       </h1>
 
-      <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {products.map((product) => (
-          <div
-            key={product.id}
-            className="bg-white rounded-2xl shadow-md p-4"
-          >
-            <img
-              src={product.image}
-              alt={product.title}
-              className="h-40 mx-auto object-contain"
-            />
+      {products.length === 0 ? (
+        <p className="text-center">Error cargando productos =( </p>
+      ) : (
+        <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {products.map((product) => (
+            <div
+              key={product.id}
+              className="bg-white rounded-2xl shadow-md p-4"
+            >
+              <img
+                src={product.image}
+                alt={product.title}
+                className="h-40 mx-auto object-contain"
+              />
 
-            <h2 className="mt-4 font-semibold text-sm">
-              {product.title}
-            </h2>
+              <h2 className="mt-4 font-semibold text-sm">
+                {product.title}
+              </h2>
 
-            <p className="text-lg font-bold text-green-600 mt-2">
-              ${product.price}
-            </p>
-          </div>
-        ))}
-      </div>
+              <p className="text-lg font-bold text-green-600 mt-2">
+                ${product.price}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
     </main>
   );
 }
